f5c66640d8fa144cef4bdf34ba17f06108ebd6f5
Zosha <posyamyka@gmail.com>
2021-08-18 22:14:15 +0300

    Задачи 1 и 2

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -18,7 +18,7 @@ public class CardOrderTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
-       // options.addArguments("--headless");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.get("http://localhost:9999");
     }
